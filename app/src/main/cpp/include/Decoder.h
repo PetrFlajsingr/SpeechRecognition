9c274bfde819313d19af4046ddf0f8831efa7701
Petr <xflajs00@stud.fit.vutbr.cz>
2018-03-25 11:55:25 +0200

    create Decoder

---
+++ app/src/main/cpp/include/Decoder.h
@@ -0,0 +1,14 @@
+//
+// Created by Petr Flajsingr on 25/03/2018.
+//
+
+#ifndef VOICERECOGNITION_DECODER_H
+#define VOICERECOGNITION_DECODER_H
+
+
+class Decoder {
+
+};
+
+
+#endif //VOICERECOGNITION_DECODER_H
