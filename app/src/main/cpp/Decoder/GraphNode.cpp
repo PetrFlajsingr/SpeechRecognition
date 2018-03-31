df832723d90a4309a2906b2118983ff225b5d399
Petr <xflajs00@stud.fit.vutbr.cz>
2018-03-31 10:45:47 +0200

    add GraphNode

---
+++ app/src/main/cpp/Decoder/GraphNode.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Petr Flajsingr on 31/03/2018.
+//
+
+#include "include/GraphNode.h"
---
+++ app/src/main/cpp/Decoder/include/GraphNode.h
@@ -0,0 +1,14 @@
+//
+// Created by Petr Flajsingr on 31/03/2018.
+//
+
+#ifndef VOICERECOGNITION_GRAPHNODE_H
+#define VOICERECOGNITION_GRAPHNODE_H
+
+
+class GraphNode {
+
+};
+
+
+#endif //VOICERECOGNITION_GRAPHNODE_H
